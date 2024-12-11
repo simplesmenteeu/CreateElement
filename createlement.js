b14b7da60dde7dc2ffbec78bed195f4eb0cc5829
rafael amaral <124887158+toxiglinter40@users.noreply.github.com>
2024-12-11 19:06:55 -0300

    HTML, CSS e JavaScript

---
+++ createlement.js
@@ -0,0 +1,10 @@
+let novadiv = document.createElement("div");
+
+document.body.appendChild(novadiv);
+
+novadiv.innerHTML = `<h1>Olá mundo</h1>`;
+novadiv.classList.add('container');
+novadiv.style.backgroundColor = 'Blue';
+
+
+   
